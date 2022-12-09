8292173629cab6f833079bffeac3b9d341764f97
KomlaRD <ankueric1@gmail.com>
2022-12-08 17:49:24 -0800

    Script to print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print using printf
+ *
+ (* 
+ * Description: Print string of text using the printf function)
+ *
+ * Return: Return value is 0
+ */
+int main(void)
+{
+	printf("with proper gramer, but the outcome is a piece of art");
+	return(0);
+}
