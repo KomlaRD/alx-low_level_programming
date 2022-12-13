b834a93c519bb6e1aaa64ea5d0265002e5d2566d
KomlaRD <ankueric1@gmail.com>
2022-12-13 20:09:13 +0000

    Remove trailing whitespace

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 /**
  * _isalpha- check for alphabetic character
  *
- (* Description: Function to check if an input is 
+(* Description: Function to check if an input is
 * an alphabet lowercase or uppercase)
 *
 * Return: 1 if input c is a letter, lowercase or uppercase, otherwise 0
