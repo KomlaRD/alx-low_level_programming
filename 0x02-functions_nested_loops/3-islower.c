8c002dafedd01e30ca723891f004ce725070b0b2
KomlaRD <ankueric1@gmail.com>
2022-12-13 18:50:06 +0000

    Print 1 if input is lowercase else print 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+/**
+ * _islower- checks for lowercase characters
+ *
+(* Description: Function checks a given input if it
+* is a lowercase character)
+* @c: input character
+*
+* Return: Always 0
+*/
+
+#include <main.h>
+
+int _islower(int c)
+{
+	if (_islower(c) == true)
+	{
+		_putchar("1\n");
+	}
+	else
+	{
+		_putchar("0\n");
+	}
+
+	return (0);
+}
