60e9a839a9771241a731081f3829ebf571a08f36
KomlaRD <ankueric1@gmail.com>
2022-12-13 20:02:44 +0000

    Check if input character is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+/**
+ * _isalpha- check for alphabetic character
+ *
+ (* Description: Function to check if an input is 
+* an alphabet lowercase or uppercase)
+*
+* Return: 1 if input c is a letter, lowercase or uppercase, otherwise 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
