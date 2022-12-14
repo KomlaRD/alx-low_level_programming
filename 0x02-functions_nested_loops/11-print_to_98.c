fd437cf5877c8da50fd2100cdcb93ee48c86b6b0
KomlaRD <ankueric1@gmail.com>
2022-12-14 04:13:51 +0000

    Fix: Add missing semicolons

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,7 +25,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d\n", i)
+				printf("%d\n", i);
 			}
 		}
 	}
@@ -39,7 +39,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				print("%d\n", i)
+				print("%d\n", i);
 			}
 		}
 	}
