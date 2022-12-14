4d48a7e4ca3347a51be245ce0c2aefd7a81ae1f1
KomlaRD <ankueric1@gmail.com>
2022-12-14 04:12:24 +0000

    Add another if statement to handle comma appearing after 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,16 +17,30 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i < 98; i++)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i)
+			}
 		}
 	}
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				print("%d\n", i)
+			}
 		}
 	}
 }
