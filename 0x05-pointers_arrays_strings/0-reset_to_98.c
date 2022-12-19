ce60d12cdfdec93a4ac7aa5dfeac909b1a8904ec
KomlaRD <ankueric1@gmail.com>
2022-12-19 20:53:39 +0000

    Reset a given integer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset a given number to 98
+ * @n: integer to modify
+ *
+ (* Description: a pointer pointing to an integar
+ * is dereferenced to 98)
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
