c689e752a9a464151f4e41c398eae80d0bc17c82
KomlaRD <ankueric1@gmail.com>
2022-12-20 05:27:37 +0000

    Print a given input of string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse order
+ *
+ * @s: string input
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int lengthOfString = 0;
+
+	for (i = lengthOfStringv - 1 ; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
