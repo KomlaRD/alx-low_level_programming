2c11e4948281b255436734ce4c9eb6a1b732af58
KomlaRD <ankueric1@gmail.com>
2022-12-20 18:05:44 +0000

    Reverse a given input of characters

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,20 +12,46 @@ void rev_string(char *s)
 	int i, lengthOfString, n;
 	char c1, c2;
 
-	for (lengthOfString = 0; s[lengthOfString] != '\0'; lengthOfString++)
-	{
-	}
-
-	n = lengthOfString - 1;
 	i = 0;
+	lengthOfString = _strlen(s) - 1;
 
-	while (n > 1)
+	while (lengthOfString > i)
 	{
-		c1 = s[i];
-		c2 = s[n];
-		s[i] = c2;
-		s[n] = c1;
-		n--;
+		swap_char(s + lengthOfString, s + i);
 		i++;
+		lengthOfString--;
 	}
+}
+
+/**
+ * _strlen - return length of string
+ *
+ * @s: string
+ *
+ * Return: length of a given input of string
+ */
+int _strlen(char *s)
+{
+	int i, j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	j++;
+	return (j);
+}
+
+/**
+ * swap_char - swap characters
+ *
+ * @c1: first character
+ * @c2: second character
+ *
+ * Return: nothing
+ */
+void swap_char(char *c1, char *c2)
+{
+	char p = *a;
+	*a = *b;
+	*b = p;
 }
